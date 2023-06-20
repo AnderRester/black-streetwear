f0bd302d30c500860d65d8279541434d3bf0c23c
dariecristian <cristian.darie13@gmail.com>
2023-06-20 14:48:03 +0300

    updates

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -9,23 +9,21 @@ const DeviceItem = ({ device }) => {
     console.log(navigate);
     return (
         <Col md={3} className={"mt-3"} onClick={() => navigate(DEVICE_ROUTE + "/" + device.id)}>
-            <Card style={{ width: 180, height: 270, cursor: "pointer" }}>
+            <Card style={{ width: 220, height: 300, cursor: "pointer" }}>
                 <Image
                     className={"align-self-center"}
                     width={"auto"}
-                    height={150}
+                    height={200}
                     src={process.env.REACT_APP_API_URL + device.img}
                 />
                 <div
                     className={"d-flex align-items-center justify-content-center text-center mt-3"}
-                    style={{ height: "100%" }}
-                >
+                    style={{ height: "100%" }}>
                     {device.name}
                 </div>
                 <div
                     className={"text-black-50 d-flex justify-content-around align-items-end mb-3"}
-                    style={{ height: "100%" }}
-                >
+                    style={{ height: "100%" }}>
                     <div style={{ fontWeight: "bold", color: "darkslategray" }}>
                         {device.price} MDL
                     </div>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -10,50 +10,44 @@ const DevicePage = () => {
         fetchOneDevice(id).then((data) => setDevice(data));
     }, []);
     return (
-        <Container className={"mt-4 justify-content-around"}>
+        <Container className={"my-5 py-5"}>
             <Row>
                 <Col md={6}>
                     <Image
                         width={"auto"}
-                        height={400}
+                        height={900}
                         src={process.env.REACT_APP_API_URL + device.img}
+                        style={{ objectFit: "cover" }}
+                        className="img-fluid"
                     />
                 </Col>
                 <Col md={6}>
-                    <Card
-                        className={"d-flex justify-content-between align-items-center"}
-                        style={{ height: "100%", border: "none" }}
-                    >
+                    <Card className={"d-flex ps-5 py-5"} style={{ border: "none" }}>
                         <Row>
-                            <h2 className={"mb-2 mt-5 text-center"}>{device.name}</h2>
+                            <h2 className={"my-3"}>{device.name}</h2>
                         </Row>
                         <Row>
-                            <h3 className={"mb-2 text-center"}>Available Sizes</h3>
+                            <h4 className={"mt-3"}>Available Sizes</h4>
                         </Row>
                         <Row>
-                            <h3 className={"text-center"}>X XL XXL</h3>
+                            <h5>X XL XXL</h5>
                         </Row>
-                        <h3>{device.price} MDL</h3>
-                        <Button className={"mb-2"} variant={"outline-dark"}>
+                        <Row className="py-3">
+                            {device.info.map((info, index) => (
+                                <Row key={info.id}>
+                                    <h5 style={{ fontWeight: 400 }}>
+                                        {info.title}: {info.description}
+                                    </h5>
+                                </Row>
+                            ))}
+                        </Row>
+                        <h3 className="mt-4 mb-3 text-center">{device.price} MDL</h3>
+                        <Button className={"my-3"} variant={"outline-dark"}>
                             Add to Cart
                         </Button>
                     </Card>
                 </Col>
             </Row>
-            {/* <Row className={"d-flex flex-column m-3"} style={{ width: "100%", fontSize: 18 }}>
-                <h1>Parameters</h1>
-                {device.info.map((info, index) => (
-                    <Row
-                        key={info.id}
-                        style={{
-                            background: index % 2 === 0 ? "lightgray" : "transparent",
-                            padding: 10,
-                        }}
-                    >
-                        {info.title}: {info.description}
-                    </Row>
-                ))}
-            </Row> */}
         </Container>
     );
 };
